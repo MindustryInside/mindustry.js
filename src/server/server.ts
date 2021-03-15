ebdc049d3da3fbb4f3c27de3aeffe57230176be9
summetdev <summet.dev@gmail.com>
2021-03-15 23:31:20 +0300

    refactor(Server): data() -> getData()

--- src/server/server.ts
+++ src/server/server.ts
@@ -11,7 +11,13 @@ export class Server extends NetConnection {
         this.host = new Host(hostname, port);
     }
 
+    /** @deprecated since 0.1.0 */
     async data(): Promise<ServerData> {
+        console.warn('Function Server#data() has been deprecated, please use Server#getData() function instead.');
+        return this.getData();
+    }
+
+    async getData(): Promise<ServerData> {
         return this.udp.connect(this.host.hostname, this.host.port)
             .then(() => this.udp.send(discoverPacket))
             .then(() => this.udp.awaitMessage())
